581ae9a0107d9ce1dddaf75c4b4f9aae69649957
HugSTapG <hugo.tapia@uees.edu.ec>
2025-10-02 17:18:35 -0500

    Init_Change_Email

--- src/main/java/labcodeinspection/Email.java
+++ src/main/java/labcodeinspection/Email.java
@@ -1,51 +1,66 @@
 package labcodeinspection;
 
+import java.util.Locale;
+
+//Clase que representa una cuenta de correo institucional
 public class Email {
 
-	private String m_firstName;
-	private String m_lastName;
-	private String password = null;
-	private String department;
-	private int defaultpasswordLength = 8;
-	private String email;
+    // Nombre del usuario
+    private final String firstName;
 
-	public Email(String firstName, String lastName) {
-		this.m_firstName = firstName;
-		this.m_lastName = lastName;
-	}
+    // Apellido del usuario
+    private final String lastName;
 
-	public void showInfo() {
-		System.out.println("\nFIRST NAME= " + m_firstName + "\nLAST NAME= " + m_lastName);
-		System.out.println("DEPARMENT= " + department + "\nEMAIL= " + email + "\nPASSWORD= " + password);
-	}
+    // Contraseña generada para el usuario
+    private String password;
 
-	public void setDeparment(int depChoice) {
-		switch (depChoice) {
-		case 1:
-			this.department = "sales";
-			break;
-		case 2:
-			this.department = "dev";
-			break;
-		case 3:
-			this.department = "acct";
-			break;
-		}
-	}
+    // Departamento asignado
+    private String department;
 
-	private String randomPassword(int length) {
-		String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
-		char[] password = new char[length];
-		for (int i = 0; i < length; i++) {
-			int rand = (int) (Math.random() * set.length());
-			password[i] = set.charAt(rand);
-		}
-		return new String(password);
-	}
+    //Longitud por defecto para la contraseña
+    private static final int DEFAULT_PASSWORD_LENGTH = 8;
 
-	public void generateEmail() {
-		this.password = this.randomPassword(this.defaultpasswordLength);
-		this.email = this.m_firstName.toLowerCase() + this.m_lastName.toLowerCase() + "@" + this.department
-				+ ".espol.edu.ec";
-	}
+    //Dirección de correo electrónico generada
+    private String email;
+
+    //Constructor de Email.
+    public Email(final String firstName, final String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //Muestra la información del email por consola.
+    public void showInfo() {
+        System.out.println("\nFIRST NAME= " + firstName + "\nLAST NAME= " + lastName);
+        System.out.println("DEPARTMENT= " + department + "\nEMAIL= " + email + "\nPASSWORD= " + password);
+    }
+
+    // Asigna un departamento según la opción seleccionada.
+    public void setDepartment(final int depChoice) {
+        switch (depChoice) {
+            case 1 -> this.department = "sales";
+            case 2 -> this.department = "dev";
+            case 3 -> this.department = "acct";
+            default -> this.department = "general";
+        }
+    }
+
+    //Genera una contraseña aleatoria.
+    private String randomPassword(final int length) {
+        final String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
+        final char[] password = new char[length];
+        for (int i = 0; i < length; i++) {
+            final int rand = (int) (Math.random() * set.length());
+            password[i] = set.charAt(rand);
+        }
+        return new String(password);
+    }
+
+    //Genera el correo institucional y asigna una contraseña.
+    public void generateEmail() {
+        this.password = this.randomPassword(DEFAULT_PASSWORD_LENGTH);
+        this.email = this.firstName.toLowerCase(Locale.ROOT) 
+                   + this.lastName.toLowerCase(Locale.ROOT)
+                   + "@" + this.department + ".espol.edu.ec";
+    }
 }
